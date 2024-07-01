ab29db5ce94d9832c212d06e3dee5789440ed7ff
John Källén <johnkal@gmail.com>
2024-07-01 13:18:06 +0200

    Code for seventh session

---
+++ ep7/rvfun/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using rvfun;
+
+using static rvfun.Mnemonics;
+
+var mem = new Memory(new byte[1024]);
+
+// int x1 = 10;
+// int x2 = 1;
+// goto check;
+
+// loop:
+// x2 = x2 * x1;
+// x1 = x1 - 1;
+
+// check:
+// if (x1 > 1) goto loop;
+
+
