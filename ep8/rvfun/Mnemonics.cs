8ec5189ae0d4d5a8e99e1e5ddaac5e70cce2d2b2
John Källén <johnkal@gmail.com>
2024-07-04 02:11:09 +0200

    Code for eighth session

---
+++ ep8/rvfun/Mnemonics.cs
@@ -0,0 +1,28 @@
+namespace rvfun;
+
+public enum Mnemonics
+{
+    Invalid, 
+    addi,
+    muli,
+    add,
+    mul,    
+
+    lb,
+    lbu,
+    sb,
+
+    ecall,
+    lh,
+    lhu,
+    lw,
+    sh,
+    sw,
+
+    jal,
+    sgti,
+    bge,
+    bne,
+    slti,
+    jalr,
+}
